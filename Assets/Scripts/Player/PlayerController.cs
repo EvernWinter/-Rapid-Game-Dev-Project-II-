9a0de3c94e0995308107a6f0fbdae6935801eee5
EvernWinter <phurin.meth@bumail.net>
2024-09-22 21:10:22 +0700

    Player-Basic-Controller
    Move,Jump

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Spine.Unity;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Movement")] //Movement value
+    [SerializeField] private float moveSpd;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float movementInput;
+    [SerializeField] private Rigidbody2D playerRb;
+    
+    [Header("GroundCheck")] //GroundCheck value
+    [SerializeField] private Transform feet;
+    [SerializeField] private bool isOnGround;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float checkRadius;
+    
+    [Header("Animation")] //Player animation value
+    [SerializeField] private AnimationReferenceAsset idle;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = this.GetComponent<Rigidbody2D>(); //Get the player rigidbody
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        movementInput = Input.GetAxis("Horizontal");
+        playerRb.velocity = new Vector2(movementInput * moveSpd,playerRb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        isOnGround = Physics2D.OverlapCircle(feet.position, checkRadius, groundLayer);
+        if (isOnGround && Input.GetButtonDown("Jump"))
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
+        }
+    }
+
+    private void OnDrawRadius()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(feet.position, checkRadius);
+    }
+
+}
